1b9e6886c61913f78be737a1e2a63511269f2136
Kelvin Barasa <officialbarasa@gmail.com>
2023-05-10 04:49:22 -0700

    Update solution to task 12

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a')-1, -1):
-    print("{}{}".format(chr(i), chr(i-32)), end="")
+    if i % 2 == 0:
+        d = 0
+    else:
+        d = 32
+    print("{}".format(chr(i-d)), end="")
