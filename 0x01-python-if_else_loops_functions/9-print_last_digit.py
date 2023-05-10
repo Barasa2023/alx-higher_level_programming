2d2296150119234104e714cc63d6a48d9ffcd717
Kelvin Barasa <officialbarasa@gmail.com>
2023-05-10 04:55:56 -0700

    print last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print("{}".format(number % 10))
+    if number < 0:
+        digit = number % -(10)
+        print("{}".format(digit), end="")
+    else:
+        digit = number % 10
+        print("{}".format(digit), end="")
+    return abs(digit)
