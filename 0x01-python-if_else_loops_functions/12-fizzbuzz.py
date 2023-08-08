9998329014d7f1ab9d0e452f7501a0495389ec15
Barasa Kelvin <officialbarasa@gmail.com>
2023-08-08 02:42:22 -0700

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,8 +3,11 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
             print(f'Fizz', end=" ")
+            continue
         if i % 5 == 0:
             print(f'Buzz', end=" ")
+            continue
         if i % 3 == 0 and i % 5 == 0:
             print(f'Fizzbuzz', end=" ")
+            continue
         print(f'{i}', end=' ')
