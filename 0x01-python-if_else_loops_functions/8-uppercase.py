ec7f9097127e7ef08b26469c6b88dcebc5a4eed6
Barasa Kelvin <officialbarasa@gmail.com>
2023-08-09 01:28:05 -0700

    Print uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for letter in str:
+        if letter = " ":
+            print(f'{letter}', end="")
         if ord(letter) in range(65, 91):
             diff = ord(letter)
         else:
