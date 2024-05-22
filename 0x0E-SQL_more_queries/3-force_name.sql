fa8fb689acb1e66cec544018cd43b1c1b9132856
Kelvin <officialbarasa@gmail.com>
2024-05-22 09:15:34 -0700

    Force name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -2,5 +2,5 @@
 -- database name will be passed as an argument of the mysql command
 CREATE TABLE IF NOT EXISTS `force_name`(
 	`id` INT,
-	`NAME` VARCHAR(256) NOT NULL
+	`name` VARCHAR(256) NOT NULL
 	);
