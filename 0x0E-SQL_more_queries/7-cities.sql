e9f2029ea21ed5ef2eccd1756166b02f5844d10f
Kelvin <officialbarasa@gmail.com>
2024-05-22 09:43:40 -0700

    Update cities

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- creates the database hbtn_0d_usa and the table cities
+-- If the database hbtn_0d_usa already exists, your script should not fail
+-- If the table cities already exists, your script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT UNIQUE AUTO_iNCREAMENT NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL FOREIGN KEY(id),
+	name VARCHAR(256)
+	)
