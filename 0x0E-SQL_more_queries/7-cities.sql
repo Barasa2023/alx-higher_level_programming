65904a0ed0c0c970bccaa973fe597699884a34e1
Kelvin <officialbarasa@gmail.com>
2024-05-22 12:26:43 -0700

    Cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,7 @@
 -- If the table cities already exists, your script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	state_id INT NOT NULL,
